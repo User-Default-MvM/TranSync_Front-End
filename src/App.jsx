35f71267a804e27413c66a4cdd0e4245aaff637c
BryanCodeDev <tiagotroller021019@gmail.com>
2025-09-25 14:29:34 -0500

    Actualizacion5

--- src/App.jsx
+++ src/App.jsx
@@ -115,7 +115,7 @@ function App() {
     <ThemeProvider>
       <AuthProvider>
         <UserProvider>
-          <Router basename="/admin">
+          <Router basename="/">
           <Suspense fallback={<LazyLoadingFallback />}>
             <Routes>
               <Route path="/" element={<Navigate to="/home" replace />} />
