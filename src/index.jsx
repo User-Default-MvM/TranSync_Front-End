7130ff2080368d4cc340eb6cd1be8b362cea17d0
Usuario de ejemplo <tiagotroller021019@gmail.com>
2025-09-26 18:18:04 -0500

    fix: corregir configuración de providers y headers adicionales para MIME types

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
 import App from './App';
-import { ThemeProvider } from "./context/ThemeContext";
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <ThemeProvider>
-    <App />
-  </ThemeProvider>
-);
+root.render(<App />);
